28dbc53abb3702e1ee52d5951e11fb12801365be
Nikita Kobylkin <sheliss.kai@gmail.com>
2022-07-29 19:14:21 +0300

    Filter works. need change flex to grid

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
+  @Output() filterClick = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFilterClick(data: string) {
+    this.filterClick.emit(data);
   }
 
 }
--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -10,14 +10,44 @@ import { Item } from 'src/app/Interfaces';
 export class ProductsListComponent implements OnInit {
   items: Item[] = [];
   shownItems: number = 8;
+  filteredItems: Item[] = [];
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
    this.shopService.getProducts().subscribe((items: Item[]) => (this.items = items));
+   this.shopService.getProducts().subscribe((items: Item[]) => (this.filteredItems = items));
   }
 
   showMore() {
     this.shownItems += 8;
+  }
+
+  onFilterClick(data: string) {
+    switch (data) {
+      case 'all': {
+        this.filteredItems = this.items;
+        break;
+      }
+      case 'men' : {
+        this.filteredItems = this.items.filter(item => item.category === 'men\'s clothing');
+        break;
+      }
+      case 'women' : {
+        this.filteredItems = this.items.filter(item => item.category === 'women\'s clothing');
+        break;
+      }
+      case 'jewel' : {
+        this.filteredItems = this.items.filter(item => item.category === 'jewelery');
+        break;
+      }
+      case 'electro' : {
+        this.filteredItems = this.items.filter(item => item.category === 'electronics');
+        break;
+      }
+      default : {
+        console.error('Unknown filter value');
+      }
+    }
   }
 
 }
